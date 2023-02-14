4d3f034849641f21991a6fa045cf3958ad1ece0b
mlopez <ml.3012@gmail.com>
2023-02-14 15:57:42 -0300

    image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import NavBar from './components/navbar/Navbar';
+import NavBar from './components/Home/navbar/Navbar';
+import FontImage from './components/Home/homeImage/Font.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <NavBar />
+        <FontImage />
         <Routes>
           <Route path='/' />
         </Routes>
---
+++ src/components/Home/homeImage/Font.jsx
@@ -0,0 +1,10 @@
+import font from "./banner.jpg"
+import "./font.css"
+
+const FontImage = () => {
+    return (
+        <img className="font"  src={font} alt= "Font"/>
+    )
+}
+
+export default FontImage;
