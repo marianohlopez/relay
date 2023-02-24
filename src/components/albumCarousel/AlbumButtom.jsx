db090b67c717f73d92a26da6fa2539db2bfea5f6
mlopez <ml.3012@gmail.com>
2023-02-24 11:30:23 -0300

    correction

--- src/components/albumCarousel/AlbumButtom.jsx
+++ src/components/albumCarousel/AlbumButtom.jsx
@@ -1,10 +1,15 @@
 import './albumCarousel.css';
 
-const AlbumButtom = ({displayPlayer}) => {
-
-    return (
-        <button onClick={displayPlayer} className="albumButton">+</button>
-    )
+const AlbumButtom = ({displayPlayer, classPlayer}) => {
+    if(classPlayer === "albumPlayerActive"){
+        return (
+            <button onClick={displayPlayer} className="albumButton">-</button>
+        )
+    }else {
+        return (
+            <button onClick={displayPlayer} className="albumButton">+</button>
+        )
+    }
 }
 
 export default AlbumButtom;
--- src/components/albumCarousel/AlbumCarousel.jsx
+++ src/components/albumCarousel/AlbumCarousel.jsx
@@ -12,15 +12,15 @@ const AlbumCarousel = () => {
 
     return (
         <div className='carouselFont'>
-            <Carousel variant="dark" indicators={false} pauseOnHover={false}>
+            <Carousel variant="dark" indicators={false}>
                 <Carousel.Item>
                     <div className="d-flex justify-content-center align-items-center"> 
-                            <img
-                                className="imageAlbum"
-                                src="./img/albumCarousel/Sarajevo.jpeg"
-                                alt="First slide"
-                            />
-                        <AlbumButtom displayPlayer={displayPlayer}/>
+                        <img
+                            className="imageAlbum"
+                            src="./img/albumCarousel/Sarajevo.jpeg"
+                            alt="First slide"
+                        />
+                        <AlbumButtom displayPlayer={displayPlayer} classPlayer={display} />
                         <iframe className={display} style={{borderRadius:'12px', zIndex: "2"}} 
                             src="https://open.spotify.com/embed/album/0a9EXZcAMNP5bIaJTOIeaD?utm_source=generator" 
                             width="40%" height="352" frameBorder="0" allowFullScreen="" 
@@ -36,7 +36,7 @@ const AlbumCarousel = () => {
                             src="./img/albumCarousel/Las-Cosas-son-Asi.png"
                             alt="Second slide"
                         />
-                        <AlbumButtom displayPlayer={displayPlayer}/>
+                        <AlbumButtom displayPlayer={displayPlayer} classPlayer={display} />
                         <iframe className={display} style={{borderRadius:'12px', zIndex: "2"}} 
                             src="https://open.spotify.com/embed/album/6s6NIePkgyzd8YRxNQO4Un?utm_source=generator" 
                             width="40%" height="352" frameBorder="0" allowFullScreen="" 
@@ -52,7 +52,7 @@ const AlbumCarousel = () => {
                         src="./img/albumCarousel/Desenredandome.jpg"
                         alt="Third slide"
                         />
-                        <AlbumButtom displayPlayer={displayPlayer}/>
+                        <AlbumButtom displayPlayer={displayPlayer} classPlayer={display} />
                         <iframe className={display} style={{borderRadius:'12px', zIndex: "2"}} 
                             src="https://open.spotify.com/embed/album/2O1Nqj57Ne8huog7PiMsaS?utm_source=generator" 
                             width="40%" height="352" frameBorder="0" allowFullScreen="" 
@@ -68,13 +68,19 @@ const AlbumCarousel = () => {
                         src="./img/albumCarousel/Quedarnos-Quietos.jpg"
                         alt="First slide"
                         />
-                        <AlbumButtom displayPlayer={displayPlayer}/>
-                        <iframe className={display} style={{borderRadius:'12px', zIndex: "2"}} 
+                        <AlbumButtom displayPlayer={displayPlayer} classPlayer={display} />
+                        <iframe className={display} style={{borderRadius:'12px'}} 
                             src="https://open.spotify.com/embed/album/6hzvfxV9xXaepasZRNPIeZ?utm_source=generator" 
                             width="40%" height="352" frameBorder="0" allowFullScreen="" 
                             allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
                             loading="lazy">
                         </iframe>
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div className="logoContainer">
+                        <img src="./img/albumCarousel/logo-rojo.png" alt="Logo rojo" />
+                        
                     </div>
                 </Carousel.Item>
             </Carousel>
